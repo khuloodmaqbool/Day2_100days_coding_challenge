d83ac7ccbfe34dd7f865a0aaff15653dee168455
khuloodmaqbool <101542490+khuloodmaqbool@users.noreply.github.com>
2024-03-11 05:45:32 -0700

    Add files via upload

---
+++ Day2.ts
@@ -0,0 +1,13 @@
+   // Question 4
+   console.log(`Allama Iqbal once said, "If faith is lost, there is no security and there is no life for him who does not adhere to religion."`);
+
+   // Question 5
+let famous_person = "Allama Iqbal"
+let message2 = `${famous_person}  once said, "If faith is lost, there is no security and there is no life for him who does not adhere to religion."`
+console.log(message2);
+
+   // Question 6
+let message3 = `\t \n khulood \n  \t`
+
+console.log(message3);
+console.log(message3.trim());
